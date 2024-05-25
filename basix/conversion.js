3cae0cffedf3edd5ac43d8af0a4157b6dbc3add0
ujjawal singh <125631135+ujjawal2601@users.noreply.github.com>
2024-05-25 20:48:01 +0000

    conversions and operations

--- basix/conversion.js
+++ basix/conversion.js
@@ -1,23 +1,23 @@
-let score="33"
-console.log(typeof score);
+// let score="33"
+// console.log(typeof score);
 
-let valueinnumber=Number(score);
-console.log(typeof valueinnumber);
+// let valueinnumber=Number(score);
+// console.log(typeof valueinnumber);
 
 
-let score2="33abc"
-console.log(typeof score2);
+// let score2="33abc"
+// console.log(typeof score2);
 
-let valueinnumber2=Number(score2);
-console.log(typeof valueinnumber2);
-console.log(valueinnumber2);
+// let valueinnumber2=Number(score2);
+// console.log(typeof valueinnumber2);
+// console.log(valueinnumber2);
 
-let score3=null
-console.log(typeof score3);
+// let score3=null
+// console.log(typeof score3);
 
-let valueinnumber3=Number(score3);
-console.log(typeof valueinnumber3);
-console.log(valueinnumber3);
+// let valueinnumber3=Number(score3);
+// console.log(typeof valueinnumber3);
+// console.log(valueinnumber3);
 
 //conversions
 //"33"=33
@@ -27,6 +27,36 @@ console.log(valueinnumber3);
 //0=false
 //""=false(bool)
 // "abc"==true(bool)
+
+// ~~~~~~~~~~~~OPERATIONS~~~~~~~~~~~
+let value=3;
+// console.log(value*2)
+// console.log(-value);
+// console.log(value**2)
+
+// let str1="ujjawal";
+// let str2=" singh";
+// let str3=str1+str2;
+// let str4="Mr " + str3;
+// console.log(str3);
+// console.log(str4);
+
+// ~~~~~~~imp~~~~~~~~
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+"2");
+// console.log(1+2+"2");
+// console.log("2"+1+1);
+
+console.log(true);
+console.log(+true);
+console.log(+"");//empty string is treated as false
+// console.log(true+);//throughs error
+
+//not a good practice
+let num1,num2,num3;
+num1 = num2 =num3 =2+2;
+
 
 
 
