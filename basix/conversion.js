0cd29c3f6eb23f454dd92d45b8d027cddcd3bba1
ujjawal singh <125631135+ujjawal2601@users.noreply.github.com>
2024-05-25 20:33:24 +0000

    conversion of datatypes

---
+++ basix/conversion.js
@@ -0,0 +1,32 @@
+let score="33"
+console.log(typeof score);
+
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber);
+
+
+let score2="33abc"
+console.log(typeof score2);
+
+let valueinnumber2=Number(score2);
+console.log(typeof valueinnumber2);
+console.log(valueinnumber2);
+
+let score3=null
+console.log(typeof score3);
+
+let valueinnumber3=Number(score3);
+console.log(typeof valueinnumber3);
+console.log(valueinnumber3);
+
+//conversions
+//"33"=33
+//"33abc"=NaN
+//true=1
+//1=true
+//0=false
+//""=false(bool)
+// "abc"==true(bool)
+
+
+
