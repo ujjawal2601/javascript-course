700bff123cd41900f871610dbb46d4b39b0e532a
ujjawal singh <125631135+ujjawal2601@users.noreply.github.com>
2024-05-25 20:21:27 +0000

    datatypes

---
+++ basix/datatypes.js
@@ -0,0 +1,24 @@
+ console.log("datatypes")
+ 
+ "use strict"; //treat all js code as newer version
+ 
+ //alert(3+3) wont run as we are using node js not a browser
+
+ console.log(3+3)
+
+ //ecma(tc39) on broswer is used for documentation in js along with mdn but original is ecma(tc39)
+
+ let name="ujjawal" //string
+ let age=22 //number
+ let isbool=false //bool
+
+ // null is a datatype(empty) and also a standalone value
+ //undefined  value is not assigned yet
+ //symbol => used as for unique and mainly in react
+ //object is also a datatype
+
+ //typeof
+ console.log(typeof "ujjawal");
+ console.log(typeof null); //its datatype is object 
+ console.log(typeof undefined);
+
