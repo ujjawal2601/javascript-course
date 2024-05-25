a0eaa99c145b6085fc9099af4dad84365a0047d2
ujjawal singh <125631135+ujjawal2601@users.noreply.github.com>
2024-05-25 19:15:10 +0000

    understanding variables

---
+++ basix/variables.js
@@ -0,0 +1,9 @@
+const acc_id=1234 //can not be change through out the program
+let email="abc@gmail"
+let name
+var pass="123" //not prefer to use var coz of scope read more
+city="jaipur" //some times we can intialise without telling the type of variables
+console.log(acc_id)
+console.log(pass)
+
+console.table([acc_id,email,pass,city,name])
