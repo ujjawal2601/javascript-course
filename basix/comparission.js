5aa1589347bc041d5d7ce6c1fa33b9901b290278
ujjawal singh <125631135+ujjawal2601@users.noreply.github.com>
2024-05-25 21:02:23 +0000

    comparissions

---
+++ basix/comparission.js
@@ -0,0 +1,26 @@
+console.log("comparission");
+
+//basic conversions
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//different data type conversions
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//avoid these type of conversions
+//null
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//undefined
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//stict check operator === 
+console.log("2" === 2);
+console.log(2 === 2);
