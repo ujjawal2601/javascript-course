b3f0b436eda29923a8e0532f957a2ef6ada87bc8
ujjawal singh <125631135+ujjawal2601@users.noreply.github.com>
2024-05-26 06:37:49 +0000

    strings

---
+++ basix/string.js
@@ -0,0 +1,40 @@
+// // declaring a string 
+// const name = "hitesh"
+// const repoCount = 50
+
+// console.log(name + repoCount + " Value"); //not a good practice of displaying a sting
+
+// //always display string in this manner
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// // declaring a string
+// const gameName = new String('hitesh-hc-com');
+
+// console.log(gameName[0]); //first letter of string
+// console.log(gameName.__proto__); //read about this or ask someone 
+
+
+// console.log(gameName.length);                //length
+// console.log(gameName.toUpperCase());         //changing to uppercase
+// console.log(gameName.charAt(2));             //finding the character at 2md index
+// console.log(gameName.indexOf('t'));          //finding the index of letter t
+
+// const newString = gameName.substring(0, 4)      //creating a newstring whil will hold the substring of gamename for the given indexing 
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// // triming a string 
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-')) //replacing certain characters
+
+// console.log(url.includes('sundar'))  //checking wether it includes or not 
+
+// console.log(gameName.split('-'));       //spliting on the basis of - which returns an array 
